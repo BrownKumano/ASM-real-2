9c3c06cb0f7875bfc5839c6ce6bbac4a63d4e959
BrownKumano <brownbdqt123@gmail.com>
2023-04-19 15:26:52 +0700

    therealasm

---
+++ ASM2/src/main/java/com/mycompany/asm2/Manga.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.asm2;
+
+public class Manga {
+
+    public static int id;
+    private String name;
+    private String genre;
+    private int quantity;
+
+    public Manga() {
+    }
+
+    public Manga(int id, String name, String genre, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.genre = genre;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+}
